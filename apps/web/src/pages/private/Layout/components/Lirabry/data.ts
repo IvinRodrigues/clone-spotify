f43a64a48734609c35f3f1c7665e968a05476cee
Ivin <ivinrodriguees@gmail.com>
2024-06-06 19:45:01 -0300

    refactor(apps/web): change in data model of songs

--- apps/web/src/pages/private/Layout/components/Lirabry/data.ts
+++ apps/web/src/pages/private/Layout/components/Lirabry/data.ts
@@ -1,43 +1,51 @@
-const musics = [
-  {
-    artist: {
-      id: 1,
-      name: "Artist name",
-      about: "About artist",
-      monthlyListeners: 10000,
-      picture:
-        "https://i.scdn.co/image/ab6761610000e5eb0d96912fd002f4e038b4e17e",
-    },
-    music: {
-      id: 1,
-      name: "Music name",
-      credits: [
-        {
-          id: 1,
-          name: "Artist single name",
-          role: "Composer",
-          mainArtist: true,
-        },
-        {
-          // artist without page in spotify
-          name: "Artist single name",
-          role: "Lyrics",
-          mainArtist: false,
-        },
-      ],
-      picture:
-        "https://cdn-p.smehost.net/sites/7f9737f2506941499994d771a29ad47a/wp-content/uploads/2021/01/957938-scaled.jpg",
-    },
-    tour: [
-      {
-        date: "02-02-2025",
-        country: "Country name",
-        description: "Event description",
-        location: "Concert hall location",
-      },
-    ],
-    lyrics: [{ text: "test", timeStamp: "0:10" }],
+const song = {
+  id: 1,
+  name: "Song name",
+  album: {
+    id: 1,
+    picture:
+      "https://cdn-p.smehost.net/sites/7f9737f2506941499994d771a29ad47a/wp-content/uploads/2021/01/957938-scaled.jpg",
   },
-];
-
-export { musics };
+  artist: {
+    id: 1,
+    name: "Artist name",
+    about: "About artist",
+    monthlyListeners: 10000,
+    picture: "https://i.scdn.co/image/ab6761610000e5eb0d96912fd002f4e038b4e17e",
+  },
+  credits: [
+    {
+      // the first is always the main artist
+      id: 1,
+      name: "Artist single name",
+      role: "Composer",
+    },
+    {
+      // no ID because the artist doesn't have a Spotify page
+      name: "Artist single name",
+      role: "Lyrics",
+    },
+  ],
+  lyrics: [
+    { text: "test", timeStamp: "0:10" },
+    { text: "test", timeStamp: "0:15" },
+    { text: "test", timeStamp: "0:20" },
+    { text: "test", timeStamp: "0:25" },
+  ],
+  tour: [
+    {
+      id: 1,
+      date: "02-02-2025",
+      country: "Country name",
+      description: "Event description",
+      location: "Concert hall location",
+    },
+    {
+      id: 2,
+      date: "02-24-2025",
+      country: "Country name",
+      description: "Event description",
+      location: "Concert hall location",
+    },
+  ],
+};
