489b30928416fd2fe07a18dcc0766c0aae7d4c1a
Ivin <ivinrodriguees@gmail.com>
2024-06-06 19:45:40 -0300

    feat(apps/web): implementation of OutletAnimationProvider

--- apps/web/src/pages/private/Layout/index.tsx
+++ apps/web/src/pages/private/Layout/index.tsx
@@ -1,19 +1,20 @@
-import { Outlet } from "react-router-dom";
-import { Grid, MainContent } from "./styles";
+import { Grid } from "./styles";
 import { Info } from "./components/Info";
 import { Player } from "./components/Player";
 import { Library } from "./components/Lirabry";
+import { OutletContent } from "./components/Outlet";
+import { OutletAnimationProvider } from "../../../contexts/outletAnimationContext";
 
 function Layout() {
   return (
-    <Grid>
-      <Library />
-      <MainContent>
-        <Outlet />
-      </MainContent>
-      <Info />
-      <Player />
-    </Grid>
+    <OutletAnimationProvider>
+      <Grid>
+        <Library />
+        <OutletContent />
+        <Info />
+        <Player />
+      </Grid>
+    </OutletAnimationProvider>
   );
 }
 
